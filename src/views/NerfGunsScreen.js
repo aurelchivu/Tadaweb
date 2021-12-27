96091c2b3d4cd4cf028fd3e216d3dc846a1a49c3
mac <aurelchivuutcb@gmail.com>
2021-12-27 22:22:46 +0200

    Delete all items displayed

--- src/views/NerfGunsScreen.js
+++ src/views/NerfGunsScreen.js
@@ -1,20 +1,56 @@
-import React, { useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import Gun from '../components/Gun';
 import useFetch from '../utils/useFetch';
 
 const NerfGunsScreen = () => {
   const params = useParams();
+  const navigate = useNavigate();
+
   const pageNumber = params.pageNumber || 1;
   const pageSizes = [4, 8, 20, 50];
   const [pageSize, setPageSize] = useState(pageSizes[3]);
+  const [deleted, setDeleted] = useState(false);
+
   const { data, pages } = useFetch(
     'http://localhost:8000/guns',
     pageNumber,
     pageSize
   );
 
-  const deleteAll = () => {};
+  const deleteAll = async (id) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      };
+      const data = await axios.delete(
+        `http://localhost:8000/guns/${id}`,
+        config
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClick = () => {
+    data.forEach((item, idx) => {
+      deleteAll(item.id);
+      if ((idx = data.length - 1)) {
+        setDeleted(true);
+      }
+    });
+    // navigate('/nerfguns/page/1');
+  };
+
+  useEffect(() => {
+    console.log(deleted);
+    deleted && navigate('/nerfguns/page/1');
+  }, [deleted, navigate]);
 
   return (
     <div className='container-fluid'>
@@ -39,13 +75,60 @@ const NerfGunsScreen = () => {
               </select>
             </div>
 
-            <button
-              onClick={deleteAll}
-              type='button'
-              className='btn btn-outline-danger'
-            >
-              Delete All
-            </button>
+            <div className='bd-highlight'>
+              <button
+                type='button'
+                className='btn btn-outline-danger'
+                data-bs-toggle='modal'
+                data-bs-target='#exampleModal'
+              >
+                Delete All
+              </button>
+
+              <div
+                className='modal fade'
+                id='exampleModal'
+                tabIndex='-1'
+                aria-labelledby='exampleModalLabel'
+                aria-hidden='true'
+              >
+                <div className='modal-dialog'>
+                  <div className='modal-content'>
+                    <div className='modal-header'>
+                      <h5 className='modal-title' id='exampleModalLabel'>
+                        Delete confirmation
+                      </h5>
+                      <button
+                        type='button'
+                        className='btn-close'
+                        data-bs-dismiss='modal'
+                        aria-label='Close'
+                      ></button>
+                    </div>
+                    <div className='modal-body'>
+                      Are you sure you want to delete all items displayed?
+                    </div>
+                    <div className='modal-footer'>
+                      <button
+                        type='button'
+                        className='btn btn-secondary'
+                        data-bs-dismiss='modal'
+                      >
+                        No! Get me out of here!
+                      </button>
+                      <button
+                        type='button'
+                        className='btn btn-outline-danger'
+                        onClick={handleClick}
+                        data-bs-dismiss='modal'
+                      >
+                        Yes! Delete all!
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className='row'>
             {data.map((item) => (
