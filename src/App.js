da5cc99d102ce8ba1e3e55778399ec44d2717185
Aurel Chivu <mac@Aurels-MacBook-Pro.local>
2022-01-17 22:06:22 +0200

    Added home path

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import AboutScreen from './views/AboutScreen';
-import AddGunScreen from './views/AddGunScreen';
-import ContactScreen from './views/ContactScreen';
-import GunEditScreen from './views/GunEditScreen';
-import GunScreen from './views/GunScreen';
-import NerfGunsScreen from './views/NerfGunsScreen';
-import './styles/main.scss';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import AboutScreen from "./views/AboutScreen";
+import AddGunScreen from "./views/AddGunScreen";
+import ContactScreen from "./views/ContactScreen";
+import GunEditScreen from "./views/GunEditScreen";
+import GunScreen from "./views/GunScreen";
+import NerfGunsScreen from "./views/NerfGunsScreen";
+import "./styles/main.scss";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <main className='py-3 container'>
+      <main className="py-3 container">
         <Routes>
-          <Route path='/about' element={<AboutScreen />} />
-          <Route path='/contact' element={<ContactScreen />} />
-          <Route path='/nerfguns' element={<NerfGunsScreen />} />
-          <Route path='/nerfguns/addgun' element={<AddGunScreen />} />
-          <Route path='/nerfguns/:id' element={<GunScreen />} />
-          <Route path='/nerfguns/:id/edit' element={<GunEditScreen />} />
+          <Route path="/about" element={<AboutScreen />} />
+          <Route path="/contact" element={<ContactScreen />} />
+          <Route path="/" element={<NerfGunsScreen />} />
+          <Route path="/nerfguns/addgun" element={<AddGunScreen />} />
+          <Route path="/nerfguns/:id" element={<GunScreen />} />
+          <Route path="/nerfguns/:id/edit" element={<GunEditScreen />} />
           <Route
-            path='/nerfguns/page/:pageNumber'
+            path="/nerfguns/page/:pageNumber"
             element={<NerfGunsScreen />}
           />
-          <Route path='*' element={<h3>Ups! No nerf gun here...</h3>} />
+          <Route path="*" element={<h3>Ups! No nerf gun here...</h3>} />
         </Routes>
       </main>
       <Footer />
--- src/views/NerfGunsScreen.js
+++ src/views/NerfGunsScreen.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import axios from 'axios';
-import { motion } from 'framer-motion';
-import Gun from '../components/Gun';
-import Meta from '../components/Meta';
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { motion } from "framer-motion";
+import Gun from "../components/Gun";
+import Meta from "../components/Meta";
 
 const NerfGunsScreen = () => {
   const params = useParams();
 
   const page = params.pageNumber || 1;
   const pageSizes = [4, 8, 20, 50];
-  let [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [pages, setPages] = useState(null);
   const [pageSize, setPageSize] = useState(pageSizes[3]);
-  const [keyword, setKeyword] = useState('');
+  const [keyword, setKeyword] = useState("");
 
-  const url = 'http://localhost:8000/guns';
+  const url = "http://localhost:8000/guns";
 
   const getData = async (url, page, pageSize) => {
     try {
       const config = {
         headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
+          "Content-Type": "application/json",
+          Accept: "application/json",
         },
       };
 
@@ -32,7 +32,7 @@ const NerfGunsScreen = () => {
       );
 
       setData(data);
-      setPages(Math.ceil(headers['x-total-count'] / pageSize));
+      setPages(Math.ceil(headers["x-total-count"] / pageSize));
     } catch (error) {
       console.log(error);
     }
@@ -46,8 +46,8 @@ const NerfGunsScreen = () => {
     try {
       const config = {
         headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
+          "Content-Type": "application/json",
+          Accept: "application/json",
         },
       };
       await axios.delete(`http://localhost:8000/guns/${id}`, config);
@@ -70,41 +70,39 @@ const NerfGunsScreen = () => {
     setKeyword(e.target.value.toLowerCase());
   };
 
-  if (keyword.length > 0) {
-    data = data.filter((u) => {
-      return u.name.toLowerCase().match(keyword);
-    });
-  }
+  const search = (gun) => {
+    return gun.filter((g) => g.name.toLowerCase().match(keyword));
+  };
 
   return (
-    <div className='container-fluid'>
-      <Meta title={'My Cool App | Nerf Guns'} />
+    <div className="container-fluid">
+      <Meta title={"My Cool App | Nerf Guns"} />
       {data && (
         <>
           <motion.div
-            className='d-flex justify-content-between'
+            className="d-flex justify-content-between"
             initial={{ translateY: -100 }}
             animate={{ translateY: 0 }}
             transition={{ duration: 1 }}
           >
-            <Link to='/nerfguns/addgun' className='btn btn-outline-success'>
+            <Link to="/nerfguns/addgun" className="btn btn-outline-success">
               Add New Gun
             </Link>
-            <form className='d-flex col-4'>
+            <form className="d-flex col-4">
               <input
-                className='form-control me-5'
-                type='search'
-                placeholder='Search guns...'
-                aria-label='Search'
+                className="form-control me-5"
+                type="search"
+                placeholder="Search guns..."
+                aria-label="Search"
                 value={keyword}
                 onChange={handleChange}
               />
             </form>
-            <div className='row'>
-              <div className='col'>
+            <div className="row">
+              <div className="col">
                 <select
-                  className='form-select form-select'
-                  aria-label='.form-select-sm example'
+                  className="form-select form-select"
+                  aria-label=".form-select-sm example"
                   defaultValue={pageSize}
                   onChange={(e) => setPageSize(e.target.value)}
                 >
@@ -115,54 +113,54 @@ const NerfGunsScreen = () => {
                   ))}
                 </select>
               </div>
-              <div className='col my-auto'>Guns/page</div>
+              <div className="col my-auto">Guns/page</div>
             </div>
-            <div className='bd-highlight'>
+            <div className="bd-highlight">
               <button
-                type='button'
-                className='btn btn-outline-danger'
-                data-bs-toggle='modal'
-                data-bs-target='#exampleModal'
+                type="button"
+                className="btn btn-outline-danger"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
               >
                 Delete All
               </button>
 
               <div
-                className='modal fade'
-                id='exampleModal'
-                tabIndex='-1'
-                aria-labelledby='exampleModalLabel'
-                aria-hidden='true'
+                className="modal fade"
+                id="exampleModal"
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
               >
-                <div className='modal-dialog'>
-                  <div className='modal-content'>
-                    <div className='modal-header'>
-                      <h5 className='modal-title' id='exampleModalLabel'>
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalLabel">
                         Delete confirmation
                       </h5>
                       <button
-                        type='button'
-                        className='btn-close'
-                        data-bs-dismiss='modal'
-                        aria-label='Close'
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
                       ></button>
                     </div>
-                    <div className='modal-body'>
+                    <div className="modal-body">
                       Are you sure you want to delete all items displayed?
                     </div>
-                    <div className='modal-footer'>
+                    <div className="modal-footer">
                       <button
-                        type='button'
-                        className='btn btn-secondary'
-                        data-bs-dismiss='modal'
+                        type="button"
+                        className="btn btn-secondary"
+                        data-bs-dismiss="modal"
                       >
                         No! Get me out of here!
                       </button>
                       <button
-                        type='button'
-                        className='btn btn-outline-danger'
+                        type="button"
+                        className="btn btn-outline-danger"
                         onClick={handleClick}
-                        data-bs-dismiss='modal'
+                        data-bs-dismiss="modal"
                       >
                         Yes! Delete all!
                       </button>
@@ -172,11 +170,11 @@ const NerfGunsScreen = () => {
               </div>
             </div>
           </motion.div>
-          <div className='row'>
-            {data.map((item, idx) => (
+          <div className="row">
+            {search(data).map((item, idx) => (
               <motion.div
                 key={item.id}
-                className='col-sm-12 col-md-6 col-lg-4 col-xl-3 d-flex align-items-stretch'
+                className="col-sm-12 col-md-6 col-lg-4 col-xl-3 d-flex align-items-stretch"
                 initial={{
                   opacity: 0,
                   translateX: idx % 2 === 0 ? 100 : -100,
@@ -194,17 +192,17 @@ const NerfGunsScreen = () => {
             ))}
           </div>
 
-          <nav aria-label='pagination'>
-            <ul className='pagination pagination-sm'>
+          <nav aria-label="pagination">
+            <ul className="pagination pagination-sm">
               {[...Array(pages).keys()].map((x) => (
                 <Link
                   key={x + 1}
                   to={`/nerfguns/page/${x + 1}`}
-                  className='btn btn-outline-primary'
+                  className="btn btn-outline-primary"
                 >
                   <li
                     className={`page-item ${
-                      params.pageNumber === x + 1 ? 'active' : ''
+                      params.pageNumber === x + 1 ? "active" : ""
                     }`}
                   >
                     {x + 1}
