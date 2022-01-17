a9c9eb59082c7d438285dc667b166f809b70bcad
aurelchivu <aurelchivuutcb@gmail.com>
2022-01-17 22:23:56 +0200

    Updated Navbar Home Path

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className='navbar sticky-top navbar-expand-lg navbar-light bg-light'>
-      <div className='container-fluid'>
-        <Link to='/nerfguns' className='navbar-brand px-1'>
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand px-1">
           MyCoolApp
         </Link>
         <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <span className='navbar-toggler-icon'></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item px-2'>
-              <Link to='/about'>About us</Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item px-2">
+              <Link to="/about">About us</Link>
             </li>
           </ul>
-          <ul className='navbar-nav ml-auto mb-2 mb-lg-0'>
-            <li className='nav-item px-2'>
-              <Link to='/contact'>Contact</Link>
+          <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+            <li className="nav-item px-2">
+              <Link to="/contact">Contact</Link>
             </li>
           </ul>
         </div>
